1c76663fa6336ab8d6cc9eeb3cc0fe0e40e0bfbd
BuildTools <unconfigured@null.spigotmc.org>
2023-05-15 17:55:31 -0400

    confing options for desltop launcher

--- desktop/src/io/github/javezki/DesktopLauncher.java
+++ desktop/src/io/github/javezki/DesktopLauncher.java
@@ -2,14 +2,15 @@ package io.github.javezki;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import io.github.javezki.BadPokemon;
-
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("ReallyBadPokemon");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
 		new Lwjgl3Application(new BadPokemon(), config);
 	}
 }
